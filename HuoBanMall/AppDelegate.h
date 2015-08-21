2ba0527d662f1ab8c7fa3bba53b11876464a7c42
sxluohaibo <lhb@lhbdeMac-mini.local>
2015-08-21 17:17:15 +0800

    Initial Commit

---
+++ HuoBanMall/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HuoBanMall
+//
+//  Created by lhb on 15/8/21.
+//  Copyright (c) 2015年 HT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HuoBanMall/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HuoBanMall
+//
+//  Created by lhb on 15/8/21.
+//  Copyright (c) 2015年 HT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
