db15110614fe785d4e6d9654aea3817a139e97a6
sxluohaibo <lhb@lhbdeMac-mini.local>
2015-08-31 09:46:56 +0800

    全局模型

---
+++ HuoBanMall/Classes/login（登录）/model/HTGlobal.m
@@ -0,0 +1,13 @@
+//
+//  HTGlobal.m
+//  HuoBanMall
+//
+//  Created by lhb on 15/8/31.
+//  Copyright (c) 2015年 HT. All rights reserved.
+//  全局数据
+
+#import "HTGlobal.h"
+
+@implementation HTGlobal
+
+@end
