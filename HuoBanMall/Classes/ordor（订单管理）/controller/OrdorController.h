23ee1fdad134f0605318894facf85ae56d15ab73
HuoTu-Mac <aladdin@aladdindeimac.local>
2015-09-15 09:25:06 +0800

    页面修改

---
+++ HuoBanMall/Classes/ordor（订单管理）/controller/OrdorController.h
@@ -0,0 +1,13 @@
+//
+//  OrdorController.h
+//  HuoBanMall
+//
+//  Created by HuoTu-Mac on 15/9/14.
+//  Copyright (c) 2015年 HT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OrdorController : NSObject
+
+@end
---
+++ HuoBanMall/Classes/ordor（订单管理）/controller/OrdorController.m
@@ -0,0 +1,15 @@
+//
+//  OrdorController.m
+//  HuoBanMall
+//
+//  Created by HuoTu-Mac on 15/9/14.
+//  Copyright (c) 2015年 HT. All rights reserved.
+//
+
+#import "OrdorController.h"
+
+@implementation OrdorController
+
+
+
+@end
